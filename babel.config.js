b2a44c16d687a50c23d3975b9b3a0c7818089c07
AnhDevit <anhdevit@gmail.com>
2020-04-18 01:10:31 +0700

    update babel

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     [
       'module-resolver',
-      ['@babel/plugin-proposal-decorators', { legacy: true }],
       {
         root: ['./'],
         alias: {
-          "*": ["./src"],
           "test/*": ["./test/"],
           '@common': './src/common',
           '@feature': './src/feature',
