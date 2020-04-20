92ee4bcb1965c0baea66b99e86e39317d99149ca
AnhDevit <anhdevit@gmail.com>
2020-04-20 22:25:37 +0700

    test menu

--- src/feature/SideBar/Flex.tsx
+++ src/feature/SideBar/Flex.tsx
@@ -7,19 +7,47 @@
  */
 
 import colors from '@common/theme/colors';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
+import { Button, Paragraph, Menu, Divider, Provider } from 'react-native-paper';
 
 export interface Props {
 }
 
 const Flex: React.FC<Props> = (props) => {
+  const [visible, setVisible] = useState(false)
+
+  const _openMenu = () => setVisible(true);
+
+  const _closeMenu = () => setVisible(false);
+
   return (
-      <View style={styles.container}>
-        <ScrollView>
-          
-        </ScrollView>
-      </View>
+    <View style={styles.container}>
+      <ScrollView>
+        <View
+          style={{
+            paddingTop: 50,
+            flexDirection: 'row',
+            justifyContent: 'center'
+          }}>
+          <Menu
+            visible={visible}
+            onDismiss={_closeMenu}
+            anchor={
+              <Button 
+              onPress={_openMenu}
+              color={'red'}
+              >Show menu</Button>
+            }
+          >
+            <Menu.Item onPress={() => { }} title="Item 1" />
+            <Menu.Item onPress={() => { }} title="Item 2" />
+            <Divider />
+            <Menu.Item onPress={() => { }} title="Item 3" />
+          </Menu>
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
