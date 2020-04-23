9f68303efa0c26bf570dc542f69356eb72d93bed
AnhDevit <anhdevit@gmail.com>
2020-04-23 22:03:21 +0700

    update

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,8 +1,5 @@
 import { combineReducers } from 'redux';
 import playgroundSlice from '../feature/Playground/playgroundSlice';
-// import { reducer as formReducer } from 'redux-form';
-
 export default combineReducers({
   playground: playgroundSlice,
-//   form: formReducer
 })
